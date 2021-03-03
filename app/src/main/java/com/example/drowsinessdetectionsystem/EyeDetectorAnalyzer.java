1f19e301f9ad51219cf4b80223bc1eaa0a494338
Pritish Chatterjee <pritishchatterjee01@gmail.com>
2021-03-03 08:47:34 +0530

    Added functionality for sending SOS SMS to emergency contact

--- app/src/main/java/com/example/drowsinessdetectionsystem/EyeDetectorAnalyzer.java
+++ app/src/main/java/com/example/drowsinessdetectionsystem/EyeDetectorAnalyzer.java
@@ -4,12 +4,22 @@ import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.face.Face;
 import com.google.mlkit.vision.face.FaceDetection;
@@ -17,20 +27,47 @@ import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
 
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class EyeDetectorAnalyzer implements ImageAnalysis.Analyzer {
     TextView textview1;
     volatile boolean stopThread=false;
     boolean startThread=true;
+    boolean startSMSThread=true;
     boolean showEyesClose=false;
+    boolean messageSent=false;
     MediaPlayer buzzer;
+    String user_emergency_contact;
+    boolean setFirebase=true;
+
     public EyeDetectorAnalyzer(TextView textview1,MediaPlayer buzzer){
         this.textview1=textview1;
         this.buzzer=buzzer;
     }
     @Override
     public void analyze(@NonNull ImageProxy imageProxy) {
+
+        if(setFirebase){
+            FirebaseAuth mAuth=mAuth=FirebaseAuth.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+            DatabaseReference reff= FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+            reff.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                    if(snapshot.hasChildren()){
+                        user_emergency_contact = snapshot.child("emergency_contact").getValue(String.class);
+                        setFirebase=false;
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                }
+            });
+        }
 
         FaceDetectorOptions highAccuracyOpts =
                 new FaceDetectorOptions.Builder()
@@ -66,6 +103,11 @@ public class EyeDetectorAnalyzer implements ImageAnalysis.Analyzer {
                                                     startThread();
                                                     if(showEyesClose){
                                                         textview1.setText("Eyes Close");
+                                                        if(messageSent==false){
+                                                            startSmsThread();
+                                                        }
+
+
 
                                                     }
                                                 }
@@ -81,8 +123,11 @@ public class EyeDetectorAnalyzer implements ImageAnalysis.Analyzer {
                                     e -> imageProxy.close());
         }
     }
-    public void startThread(){
 
+
+
+
+    public void startThread(){
         WaitForEyesThread runnable1=new WaitForEyesThread();
         Thread waitThread=new Thread(runnable1);
         stopThread=false;
@@ -92,8 +137,8 @@ public class EyeDetectorAnalyzer implements ImageAnalysis.Analyzer {
         }
     }
 
+    //Thread for Buzzer
     class WaitForEyesThread implements Runnable {
-
         int count=4;
         @Override
         public void run() {
@@ -108,6 +153,53 @@ public class EyeDetectorAnalyzer implements ImageAnalysis.Analyzer {
                     showEyesClose=true;
                     buzzer.start();
                     return;
+                }
+                Log.i("Thread Count","Count:"+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void startSmsThread(){
+        stopThread=false;
+        sendSmsThread runnable2=new sendSmsThread();
+        Thread waitThread2=new Thread(runnable2);
+        if(startSMSThread){
+            waitThread2.start();
+            startSMSThread=false;
+        }
+    }
+
+    //Thread for sending message
+    class sendSmsThread implements Runnable {
+        int count=10;
+        @Override
+        public void run() {
+            for(int i=0;i<count;i++){
+                if(stopThread){
+                    startSMSThread=true;
+                    showEyesClose=false;
+                    return;
+                }
+                if(i==count-1){
+                    try{
+                        SmsManager manager=SmsManager.getDefault();
+                        manager.sendTextMessage(user_emergency_contact,null,"I need assistance",null,null);
+                        Log.i("Eye Detector Activity","Message sent");
+                        messageSent=true;
+                        startSMSThread=false;
+                        return;
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+
+
+                    }
+                return;
                 }
                 Log.i("Thread Count","Count:"+i);
                 try {
--- app/src/main/java/com/example/drowsinessdetectionsystem/UserPageActivity.java
+++ app/src/main/java/com/example/drowsinessdetectionsystem/UserPageActivity.java
@@ -1,11 +1,16 @@
 package com.example.drowsinessdetectionsystem;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
@@ -21,18 +26,33 @@ public class UserPageActivity extends AppCompatActivity {
     Button user_details,start_monitoring;
     SwitchCompat cameraSwitch;
     String useCamera="Front Camera";
-    @SuppressLint("SetTextI18n")
+    Spinner sounds_list;
+    @SuppressLint({"SetTextI18n", "ResourceAsColor"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
         user_name_holder=findViewById(R.id.user_name_holder);
         mAuth = FirebaseAuth.getInstance();
+        sounds_list=findViewById(R.id.alert_settings);
         progressDialog = new SpotsDialog(this, R.style.Custom);
         start_monitoring=findViewById(R.id.start_monitoring);
         signout_button=findViewById(R.id.signout_button);
         cameraSwitch=findViewById(R.id.camera_switch);
         user_details=findViewById(R.id.user_details);
+
+        /*ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sounds_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner*/
+
+
+        String sounds[]=new String[]{"Buzzer","Alarm","Male Voice","Female Voice"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.custom_spinner,sounds);
+        sounds_list.setPopupBackgroundResource(R.color.bright_orange);
+        sounds_list.setAdapter(adapter);
 
         cameraSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if(isChecked){
@@ -57,12 +77,19 @@ public class UserPageActivity extends AppCompatActivity {
         });
 
         start_monitoring.setOnClickListener(v -> {
-            Intent intent1=new Intent(getApplicationContext(),MonitoringActivity.class);
-            intent1.putExtra("Camera",useCamera);
-            startActivity(intent1);
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent1 = new Intent(getApplicationContext(), MonitoringActivity.class);
+                    intent1.putExtra("Camera", useCamera);
+                    startActivity(intent1);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.SEND_SMS}, 1);
+                }
+            }
         });
     }
-    @SuppressLint("SetTextI18n")
+
+
     @Override
     public void onStart() {
         super.onStart();
